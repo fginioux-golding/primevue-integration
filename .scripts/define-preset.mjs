5f81f9c1a127ebf77a97eccf580c362d6dec9063
Frederic Ginioux <Ginioux@goldingcapital.com>
2024-07-04 13:21:10 +0000

    chore: ci-update preset file generation

--- .scripts/define-preset.mjs
+++ .scripts/define-preset.mjs
@@ -1,1 +1,34 @@
-console.log(">>>>>>> define preset <<<<<<<<<");
+import path from 'path';
+import { exec, execSync } from 'child_process';
+import { fileURLToPath } from 'url';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+
+
+// Get the list of the dockerized apps
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootDir = path.join(__dirname, '..');
+const __targetDir = path.join(rootDir, 'packages', 'libs', 'themes-preset');
+
+if (existsSync(__targetDir)) {
+  let template = readFileSync(path.join(__targetDir, 'src', 'template.txt'), 'utf-8');
+  template = `${template.replace('%{replacement}%', JSON.stringify({
+    date: new Date().toISOString(),
+  }, null, 2))}`;
+
+  const filePath = path.join(__targetDir, 'src', 'lib', 'themes-preset.ts');
+  writeFileSync(filePath, template, {
+    encoding: 'utf-8',
+  });
+
+  const msg = execSync(`git log -1 --oneline`).toString();
+  if (msg.includes('chore(CI)')) {
+
+  }
+
+  // execSync(`npx nx format:write --write ${filePath}`);
+  // execSync(`git commit -am "chore: update preset file"`);
+}
+
+
+
--- packages/libs/themes-preset/src/lib/themes-preset.spec.ts
+++
@@ -1,7 +0,0 @@
-import { themesPreset } from './themes-preset';
-
-describe('themesPreset', () => {
-  it('should work', () => {
-    expect(themesPreset()).toEqual('themes-preset');
-  });
-});
