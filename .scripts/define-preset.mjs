c5088bae30d48bd423f031cc0d61aa78f5a7c301
Frederic Ginioux <Ginioux@goldingcapital.com>
2024-07-04 14:29:53 +0000

    choreL format code

--- .scripts/define-preset.mjs
+++ .scripts/define-preset.mjs
@@ -3,7 +3,6 @@ import { exec, execFile, execSync } from 'child_process';
 import { fileURLToPath } from 'url';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 
-
 // Get the list of the dockerized apps
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,19 +10,28 @@ const rootDir = path.join(__dirname, '..');
 const __targetDir = path.join(rootDir, 'packages', 'libs', 'themes-preset');
 
 if (existsSync(__targetDir)) {
-  let template = readFileSync(path.join(__targetDir, 'src', 'template.txt'), 'utf-8');
-  template = `${template.replace('%{replacement}%', JSON.stringify({
-    date: new Date().toISOString(),
-  }, null, 2))}`;
+  let template = readFileSync(
+    path.join(__targetDir, 'src', 'template.txt'),
+    'utf-8'
+  );
+  template = `${template.replace(
+    '%{replacement}%',
+    JSON.stringify(
+      {
+        date: new Date().toISOString(),
+      },
+      null,
+      2
+    )
+  )}`;
 
   const filePath = path.join(__targetDir, 'src', 'lib', 'themes-preset.ts');
   writeFileSync(filePath, template, {
     encoding: 'utf-8',
   });
 
+  execSync(`npx nx reset`);
+  execSync(`npx nx format:write`);
   execSync(`git add ${filePath}`);
   execSync(`git commit -am "chore: ci-update preset file generation"`);
 }
-
-
-
