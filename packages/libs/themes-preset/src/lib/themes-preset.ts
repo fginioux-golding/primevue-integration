d730faac4b028f2b8e944cc3ea6ac515281ebf1f
Frederic Ginioux <Ginioux@goldingcapital.com>
2024-07-05 12:43:04 +0000

    chore: ci-update preset file generation

--- packages/libs/themes-preset/src/lib/themes-preset.ts
+++ packages/libs/themes-preset/src/lib/themes-preset.ts
@@ -1,3 +1,9 @@
+/**
+ * Generated file content...
+ * Don't update manually that file !
+ * See Readme.md documentation for more informations.
+ */
+
 import { definePreset } from '@primevue/themes';
 // No default export with access specifier "export" for the module
 // Listen to updates on Primevue documentation v4.0.0
@@ -6,5 +12,5 @@ import * as Aura from '@primevue/themes/aura';
 
 // No working just an example
 export default definePreset(Aura, {
-  "date": "2024-07-05T12:28:32.672Z"
+  date: '2024-07-05T12:43:01.452Z',
 });
